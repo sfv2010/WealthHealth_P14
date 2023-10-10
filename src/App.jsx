b3600775e99a2fb41cb2c27ccd930af4a2b6d02c
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-10 11:15:55 +0200

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import EmployeeList from "./pages/EmployeeList";
+import Error404 from "./pages/Error404";
+import Header from "./components/Header";
 
 function App() {
     return (
-        <>
-            <h1>Vite + React</h1>
-        </>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="employee" element={<EmployeeList />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
