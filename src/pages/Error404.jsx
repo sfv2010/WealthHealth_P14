f9449c237363951ef5617d16190d39e14fda5a5d
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-10 11:19:10 +0200

    Add Error404.jsx

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+/**
+ * The Page404 component displays a 404 error page if the requested page cannot be found.
+ * There is also a link back to the home page.
+ *
+ * @returns {JSX.Element} The rendered Page404 component
+ */
+
+function Error404() {
+    return (
+        <div className="mt-2 w-full h-screen flex flex-col justify-evenly items-center bg-black text-white">
+            <img
+                src={"../../public/404.png"}
+                alt="Erreur 404"
+                className="w-9/12 md:w-5/12"
+            />
+            <div className="text-sm font-semibold sm:text-xl">
+                <h2 className="-mt-10 mb-6 text-center">
+                    Oups! Page non trouvée
+                </h2>
+                <Link to="/">
+                    <p className=" cursor-pointer transition-transform hover:scale-110 hover:text-red-600 underline">
+                        Retourner sur la page d’accueil
+                    </p>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Error404;
