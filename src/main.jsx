c9f41e59f10a23bcac1897cc8b49eca996bacaa9
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-19 10:43:36 +0200

    Add <EmployeeProfileProvider>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { EmployeeProfileProvider } from "./context/EmployeeProfileContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <EmployeeProfileProvider>
+            <App />
+        </EmployeeProfileProvider>
+    </React.StrictMode>
+);
