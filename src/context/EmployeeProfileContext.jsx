80a1028a22fcb9048d4bc75666f19442fc921ea5
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-19 10:46:32 +0200

    Add EmployeeProfileContext.jsx

---
+++ src/context/EmployeeProfileContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const EmployeeProfileContext = createContext();
+
+export const useEmployeeProfile = () => {
+    return useContext(EmployeeProfileContext);
+};
+
+export const EmployeeProfileProvider = ({ children }) => {
+    const [employeeProfile, setEmployeeProfile] = useState({});
+
+    return (
+        <EmployeeProfileContext.Provider
+            value={{ employeeProfile, setEmployeeProfile }}>
+            {children}
+        </EmployeeProfileContext.Provider>
+    );
+};
+EmployeeProfileProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
