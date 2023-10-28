5854597cc901e09c96d6ee7e69de84ee793809de
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-28 17:50:06 +0200

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
                 lato: ["lato", "serif"],
                 youngSerif: ["Young Serif", "serif"],
             },
+            colors: {
+                custom: {
+                    500: "#5a6f08", // カスタムカラー
+                    501: "#E4E7D7",
+                    502: "#8B9A52",
+                },
+            },
         },
     },
     plugins: [],
