02c674816e500ec5a3893844cdd5605efa290338
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-28 17:52:53 +0200

    Add data

---
+++ src/data/table.js
@@ -0,0 +1,47 @@
+export const columns = [
+    { Header: "First Name", accessor: "FirstName" },
+    { Header: "Last Name", accessor: "LastName" },
+    { Header: "Start Date", accessor: "StartDate" },
+    { Header: "Department", accessor: "Department" },
+    { Header: "Date of Birth", accessor: "DateBirth" },
+    { Header: "Street", accessor: "Street" },
+    { Header: "City", accessor: "City" },
+    { Header: "State", accessor: "State" },
+    { Header: "Zip Code", accessor: "ZipCode" },
+];
+
+export const data = [
+    {
+        FirstName: "Ken",
+        LastName: "Smith",
+        StartDate: "10/13/2000",
+        Department: "m",
+        DateBirth: "10/21/2000",
+        Street: "brad",
+        City: "New York",
+        State: "New York",
+        ZipCode: "99110",
+    },
+    {
+        FirstName: "Kenzo",
+        LastName: "Smithm",
+        StartDate: "11/30/2020",
+        Department: "Finance",
+        DateBirth: "10/21/2001",
+        Street: "143 brad",
+        City: "Boston",
+        State: "MA",
+        ZipCode: "11110",
+    },
+    {
+        FirstName: "Szo",
+        LastName: "Vmithm",
+        StartDate: "05/09/2023",
+        Department: "Finance",
+        DateBirth: "10/21/1999",
+        Street: "143 brad",
+        City: "Boston",
+        State: "MA",
+        ZipCode: "21110",
+    },
+];
