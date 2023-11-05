c389bc6fa321ccf72bf431282b4645ff1d3511f2
Sayaka Vincent <sfv2010@hotmail.fr>
2023-11-05 13:31:47 +0100

    Fix Css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Form from "../components/Form/CreateForm";
 function Home() {
     return (
         <main className="mt-10 mx-10 md:mx-20">
-            <h2 className="text-3xl text-green-700 font-bold text-center font-youngSerif">
+            <h2 className="text-[27px] sm:text-3xl text-green-700 font-bold text-center font-youngSerif">
                 Create Employee
             </h2>
 
