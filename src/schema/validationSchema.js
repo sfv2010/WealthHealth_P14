ecd435a85d9b64dc06f1c6a568f3753a7a9ed909
Sayaka Vincent <sfv2010@hotmail.fr>
2023-11-05 13:31:23 +0100

    Fix from uppercase to lowercase

--- src/schema/validationSchema.js
+++ src/schema/validationSchema.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const schema = yup.object().shape({
-    FirstName: yup
+    firstName: yup
         .string()
         .required("First Name is required")
         .matches(
@@ -10,7 +10,7 @@ export const schema = yup.object().shape({
         )
         .max(30)
         .min(2),
-    LastName: yup
+    lastName: yup
         .string()
         .required("Last Name is required")
         .matches(
@@ -20,7 +20,7 @@ export const schema = yup.object().shape({
         .max(30)
         .min(2),
 
-    DateBirth: yup
+    dateOfBirth: yup
         .string()
         .required("Date of Birth is required. Date must be in the past.")
         .max(new Date(), "Date must be in the past")
@@ -28,7 +28,7 @@ export const schema = yup.object().shape({
             /^(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])\/\d{4}$/,
             "Please enter your date of birth in mm/dd/yyyy format"
         ),
-    StartDate: yup
+    startDate: yup
         .string()
         .required("Start Date is required. Date must be in the past.")
         .max(new Date(), "Date must be in the past")
@@ -36,13 +36,13 @@ export const schema = yup.object().shape({
             /^(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])\/\d{4}$/,
             "Please enter your start date in mm/dd/yyyy format"
         ),
-    Department: yup.string().required("Department is required"),
+    department: yup.string().required("Department is required"),
 
-    Street: yup.string().required("Street is required").max(50).min(2),
-    City: yup.string().required("City is required").max(50).min(2),
-    State: yup.string().required("State is required"),
+    street: yup.string().required("Street is required").max(50).min(2),
+    city: yup.string().required("City is required").max(50).min(2),
+    state: yup.string().required("State is required"),
 
-    ZipCode: yup
+    zipCode: yup
         .string()
         .matches(/^[0-9]{5}$/, "Zip code must be a 5-digit number")
         .required("Zip Code is required"),
