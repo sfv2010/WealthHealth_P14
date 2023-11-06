ca45c52535de286dbb113ffd54ffdaa87232e0d4
Sayaka Vincent <sfv2010@hotmail.fr>
2023-11-06 11:47:26 +0100

    Fix for deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import logoWH from "../assets/logoWH.png";
 
 function Header() {
     const location = useLocation();
@@ -13,13 +14,14 @@ function Header() {
 
             <div className="w-20 md:w-32 mx-4">
                 <img
-                    src={"../../public/logoWH.png"}
-                    alt="Logo de Wealth Health"
+                    src={logoWH}
+                    alt="A logo with a collection of olive-colored leaves"
                     className="flex-1 "
                 />
             </div>
             <div className="flex-1 flex justify-center  flex-nowrap min-w-full md:justify-end sm:min-w-0 ">
                 <button className="text-1xl text-custom-502 font-youngSerif p-2 px-4 rounded-md  md:text-2xl  transition-all  duration-300 hover:bg-custom-502 hover:text-white">
+                    {" "}
                     {location.pathname === "/employee" ? (
                         <Link to="/">Home</Link>
                     ) : (
