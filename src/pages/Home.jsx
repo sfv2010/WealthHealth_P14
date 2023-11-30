819a33fb51ddc44cad36c63814bd0b431a070822
Sayaka Vincent <sfv2010@hotmail.fr>
2023-11-30 10:28:48 +0100

    Add Modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Form from "../components/Form/CreateForm";
-
+import Modal from "my-react-modal-sv";
+import "my-react-modal-sv/dist/style.css";
 function Home() {
-    return (
-        <main className="mt-10 mx-10 md:mx-20">
-            <h2 className="text-[27px] sm:text-3xl text-green-700 font-bold text-center font-youngSerif">
-                Create Employee
-            </h2>
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-            <Form />
-            <div id="confirmation" className="modal">
-                Employee Created!
-            </div>
-        </main>
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div>
+            <main className="mt-10 mx-10 md:mx-20">
+                <h2 className="text-[27px] sm:text-3xl text-green-700 font-bold text-center font-youngSerif">
+                    Create Employee
+                </h2>
+
+                <Form setModalOpen={setIsModalOpen} />
+            </main>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <h1>Employee created !</h1>
+            </Modal>
+        </div>
     );
 }
 
