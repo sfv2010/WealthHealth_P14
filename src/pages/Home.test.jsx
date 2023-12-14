49b2bc5c76e1218d1287e1f897af7b62c7de7c6c
Sayaka Vincent <sfv2010@hotmail.fr>
2023-12-14 11:29:45 +0100

    Modifier le test

--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -1,27 +1,59 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 import { describe, expect, test, vi } from "vitest";
 import Home from "./Home";
-// import Modal from "my-react-modal-sv";
 import customRender from "../utils/customRender";
 
 describe("Test Home Component", () => {
+    // const mockCloseModal = vi.fn();
+    // vi.mock("my-react-modal-sv", () => ({
+    //     __esModule: true,
+    //     default: ({ isOpen }) => {
+    //         // isOpenとonCloseを渡してもよいが、今回はonCloseだけ使用
+    //         return isOpen ? <div>Mock Modal</div> : null;
+    //     },
+    // }));
     test("is rendered correctly", async () => {
-        const mockModal = {
-            isOpen: false,
-            onClose: vi.fn(),
-            children: "Employee created !",
-        };
-
-        // vi.mock("my-react-modal-sv", () => ({
-        //     __esModule: true,
-        //     default: mockModal.onClose,
-        //     isOpen: mockModal.isOpen,
-        //     onClose: mockModal.onClose,
-        //     children: mockModal.children,
-        // }));
-        console.log(mockModal);
         customRender(<Home />);
+        console.log(Home);
         expect(screen.getByText("Create Employee")).toBeInTheDocument();
-        expect(mockModal.isOpen).toBe(false);
+    });
+
+    // test("onClose function is called when modal is closed", () => {
+    //     const { getByText } = customRender(<Home />);
+
+    //     const formComponent = screen.getByTestId("create-employee");
+    //     fireEvent.submit(formComponent);
+    //     fireEvent.click(getByText("×"));
+
+    //     expect(mockCloseModal).toHaveBeenCalled();
+    // });
+
+    //   vi.mock('my-react-modal-sv', () => ({
+    // 	__esModule: true,
+    // 	default: ({ isOpen, onClose }) => {
+    // 	  // isOpenとonCloseを渡してもよいが、今回はonCloseだけ使用
+    // 	  return isOpen ? <div>Mock Modal</div> : null;
+    // 	},
+    //   }));
+
+    test("opens and closes the modal", async () => {
+        const { getByText, queryByText } = customRender(<Home />);
+
+        // modal must initially closed
+        expect(queryByText(/Employee created !/i)).toBe(null);
+
+        // "Create Employee" heading is drawn first
+        expect(getByText(/Create Employee/i)).toBeInTheDocument();
+
+        const formComponent = screen.getByTestId("create-employee");
+        fireEvent.submit(formComponent);
+
+        await waitFor(() => {
+            expect(getByText(/Employee created !/i)).toBeInTheDocument();
+        });
+
+        expect(queryByText(/Create Employee/i)).toBeInTheDocument();
+        fireEvent.click(getByText("×"));
+        expect(queryByText(/Employee created !/i)).toBe(null);
     });
 });
