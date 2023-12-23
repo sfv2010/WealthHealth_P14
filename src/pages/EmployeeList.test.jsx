3db62aa8308f1aff0fb15465d2604caa8b3277da
Sayaka Vincent <sfv2010@hotmail.fr>
2023-12-23 02:21:20 +0100

    Update test

--- src/pages/EmployeeList.test.jsx
+++ src/pages/EmployeeList.test.jsx
@@ -1,13 +1,13 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { beforeEach, describe, expect, test } from "vitest";
 import EmployeeList from "./EmployeeList";
 import customRender from "../utils/customRender";
-import { EmployeeProfileProvider } from "../context/EmployeeProfileContext";
-// import { data } from "../data/table";
+import { data } from "../data/table";
+import { EmployeeProfileContext } from "../context/EmployeeProfileContext";
 
 describe("Test EmployeeList Component", () => {
     beforeEach(() => {
-        customRender(<EmployeeList />, []);
+        customRender(<EmployeeList />, data);
     });
 
     test("is rendered correctly", async () => {
@@ -29,23 +29,20 @@ describe("Test EmployeeList Component", () => {
         const smithCell = smithCells[0];
         expect(smithCell).toBeInTheDocument();
     });
-
-    test("handles search functionality correctly with empty data", () => {
-        const searchInput = screen.getByPlaceholderText("Search by ...");
-        fireEvent.change(searchInput, { target: { value: "Ren" } });
-
-        const kenCells = screen.queryAllByText("Ren");
-        expect(kenCells.length).toBe(0);
-    });
 });
-test("handles search functionality correctly with context data", async () => {
-    const { rerender } = render(
-        <EmployeeProfileProvider>
-            <EmployeeList />
-        </EmployeeProfileProvider>
-    );
-    // EmployeeProfileContext.Providerから値を取得
 
+test("handles search functionality correctly with empty data", () => {
+    customRender(<EmployeeList />, []);
+    console.log(document.body.innerHTML);
+    const searchInput = screen.getByPlaceholderText("Search by ...");
+    fireEvent.change(searchInput, { target: { value: "Ren" } });
+
+    const kenCells = screen.queryAllByText("Ren");
+    expect(kenCells.length).toBe(0);
+});
+
+test("handles search functionality correctly with mock data", async () => {
+    // Mock data
     const mockEmployeeProfileData = [
         {
             firstName: "Ren",
@@ -59,32 +56,33 @@ test("handles search functionality correctly with context data", async () => {
             zipCode: "88110",
         },
     ];
-    rerender(
-        <EmployeeProfileProvider>
-            <EmployeeList employeeProfile={mockEmployeeProfileData} />
-        </EmployeeProfileProvider>
+
+    // Mock context value
+    const mockContextValue = {
+        employeeProfile: mockEmployeeProfileData,
+    };
+
+    // Render the component with the mocked context value
+    render(
+        <EmployeeProfileContext.Provider value={mockContextValue}>
+            <EmployeeList />
+        </EmployeeProfileContext.Provider>
     );
 
     const searchInput = screen.getByPlaceholderText("Search by ...");
-
-    fireEvent.change(searchInput, { target: { value: "Ren" } });
+    await waitFor(() => {
+        fireEvent.change(searchInput, { target: { value: "Ren" } });
+        expect(screen.getByText("Ren")).toBeInTheDocument();
+    });
 
     const testId = await screen.findAllByTestId("tr-employee");
-
-    console.log(testId);
     expect(testId).toHaveLength(1);
-    // expect(await screen.findByText("Ren")).toBeInTheDocument();
 
-    // fireEvent.change(searchInput, { target: { value: "Ken" } });
-    // const kenCells = screen.getAllByText("Ken");
-    // const desiredCell = kenCells[0];
-    // expect(desiredCell).toBeInTheDocument();
+    // Test sorting by a column
+    const sortByFirstName = screen.getByText("First Name");
+    fireEvent.click(sortByFirstName);
 
-    // const salesCells = screen.getAllByText("Sales");
-    // const salesCell = salesCells[0];
-    // expect(salesCell).toBeInTheDocument();
-
-    // const smithCells = screen.getAllByText("Smith");
-    // const smithCell = smithCells[0];
-    // expect(smithCell).toBeInTheDocument();
+    await waitFor(() => {
+        expect(screen.getByText("Ren")).toBeInTheDocument();
+    });
 });
--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -1,32 +1,52 @@
 import { fireEvent, screen, waitFor } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import Home from "./Home";
 import customRender from "../utils/customRender";
 
 describe("Test Home Component", () => {
-    test("is rendered correctly", async () => {
+    beforeEach(() => {
         customRender(<Home />);
+    });
+
+    test("is rendered correctly", async () => {
         expect(screen.getByText("Create Employee")).toBeInTheDocument();
     });
 
     test("opens and closes the modal", async () => {
-        const { getByText, queryByText } = customRender(<Home />);
+        const modalTestId = screen.getByTestId("my-react-modal-container");
+        const firstName = screen.getByTestId("firstName");
+        const lastName = screen.getByTestId("lastName");
+        const dateOfBirth = screen.getAllByRole("textbox")[2];
+        const startDate = screen.getAllByRole("textbox")[3];
+        const department = screen.getByTestId("department");
+        const street = screen.getByTestId("street");
+        const city = screen.getByTestId("city");
+        const state = screen.getByTestId("state");
+        const zipCode = screen.getByTestId("zipCode");
+        const formComponent = screen.getByTestId("create-employee");
 
         // modal must initially closed
-        expect(queryByText(/Employee created !/i)).toBe(null);
+        expect(modalTestId).toHaveClass("opacity-0");
 
-        // "Create Employee" heading is drawn first
-        expect(getByText(/Create Employee/i)).toBeInTheDocument();
+        fireEvent.change(firstName, { target: { value: "lena" } });
+        fireEvent.change(lastName, { target: { value: "Smith" } });
+        fireEvent.change(dateOfBirth, { target: { value: "11/29/1990" } });
+        fireEvent.change(startDate, { target: { value: "11/29/2020" } });
+        fireEvent.change(department, { target: { value: "Sales" } });
+        fireEvent.change(street, { target: { value: "12 street" } });
+        fireEvent.change(city, { target: { value: "New York" } });
+        fireEvent.change(state, { target: { value: "AL" } });
+        fireEvent.change(zipCode, { target: { value: "77250" } });
 
-        const formComponent = screen.getByTestId("create-employee");
+        // Submit the form to open the modal
         fireEvent.submit(formComponent);
-
+        // Modal must be open
         await waitFor(() => {
-            expect(getByText(/Employee created !/i)).toBeInTheDocument();
+            expect(modalTestId).toHaveClass("opacity-100");
         });
-
-        expect(queryByText(/Create Employee/i)).toBeInTheDocument();
-        fireEvent.click(getByText("×"));
-        expect(queryByText(/×/i)).toBe(null);
+        // Close the modal by clicking the close button (×)
+        fireEvent.click(screen.getByText("×"));
+        // Modal must be closed
+        expect(modalTestId).toHaveClass("opacity-0");
     });
 });
