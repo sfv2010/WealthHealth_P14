2f28d25d8a82a071426f24a4366bde504ca38094
Sayaka Vincent <sfv2010@hotmail.fr>
2024-01-03 17:36:45 +0100

    Modified for responsiveness

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -64,114 +64,118 @@ function EmployeeList() {
     );
 
     return (
-        <div className="my-5 sm:my-20 mx-4 lg:mx-10 xl:mx-32 font-lato">
-            <h1 className="text-2xl sm:text-3xl text-green-700 font-bold text-center font-youngSerif mb-5 sm:mb-20">
-                Current Employees
-            </h1>
-            <div className="sm:flex justify-between items-center py-3">
-                <PaginationSelect
-                    pageSize={pageSize}
-                    setPageSize={setPageSize}
-                />
-                <Search
-                    searchValue={searchValue}
-                    setSearchValue={setSearchValue}
-                    setGlobalFilter={setGlobalFilter}
-                />
-            </div>
+        <div className="mx-auto max-w-[2500px]">
+            <div className=" my-5 sm:my-20 mx-4 lg:mx-10 xl:mx-32 font-lato">
+                <h1 className="text-2xl sm:text-3xl text-green-700 font-bold text-center font-youngSerif mb-5 sm:mb-20">
+                    Current Employees
+                </h1>
+                <div className="sm:flex justify-between items-center py-3">
+                    <PaginationSelect
+                        pageSize={pageSize}
+                        setPageSize={setPageSize}
+                    />
+                    <Search
+                        searchValue={searchValue}
+                        setSearchValue={setSearchValue}
+                        setGlobalFilter={setGlobalFilter}
+                    />
+                </div>
 
-            <div className="overflow-x-auto shadow-md">
-                <table
-                    {...getTableProps()}
-                    role="table"
-                    tabIndex={0}
-                    className="table-fixed border-solid border-2 border-green-800 border-spacing-0 shadow-2xl box-border ">
-                    <thead>
-                        {headerGroups.map((headerGroup, headerGroupIndex) => (
-                            <tr
-                                key={headerGroupIndex}
-                                {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map(
-                                    (column, columnIndex) => (
-                                        <th
-                                            tabIndex={0}
-                                            className="border-solid border-2 border-green-800 bg-custom-500 text-white py-1 pl-2 w-[100px] lg:w-[130px] text-xs lg:text-base whitespace-nowrap"
-                                            key={columnIndex}
-                                            {...column.getHeaderProps(
-                                                column.getSortByToggleProps()
-                                            )}
-                                            aria-label={`Sort by ${column.render(
-                                                "Header"
-                                            )}`}
-                                            aria-sort={
-                                                column.isSorted
-                                                    ? column.isSortedDesc
-                                                        ? "descending"
-                                                        : "ascending"
-                                                    : "none"
-                                            }>
-                                            {column.render("Header")}{" "}
-                                            <button
-                                                className="text-sm text-custom-502 pl-1"
-                                                aria-label={`Sort by ${
-                                                    column.isSorted
-                                                        ? column.isSortedDesc
-                                                            ? "descending"
-                                                            : "ascending"
-                                                        : "ascending"
-                                                }`}>
-                                                {!column.isSorted && (
-                                                    <i className="fa fa-sort"></i>
-                                                )}
-                                                {column.isSorted &&
-                                                    !column.isSortedDesc &&
-                                                    "🔼"}
-                                                {column.isSorted &&
-                                                    column.isSortedDesc &&
-                                                    "🔽"}
-                                            </button>
-                                        </th>
-                                    )
-                                )}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {page.map((row, rowIndex) => {
-                            prepareRow(row);
-                            return (
-                                <tr
-                                    data-testid="tr-employee"
-                                    tabIndex={0}
-                                    className="even:bg-custom-501 hover:bg-custom-503 hover:text-white "
-                                    key={rowIndex}
-                                    {...row.getRowProps()}>
-                                    {row.cells.map((cell, cellIndex) => {
-                                        return (
-                                            <td
-                                                className="text-center border-solid border-2 border-green-800 p-1 text-xs lg:text-base truncate ..."
-                                                key={cellIndex}
-                                                {...cell.getCellProps()}>
-                                                {cell.render("Cell")}
-                                            </td>
-                                        );
-                                    })}
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                <div className="overflow-x-auto ">
+                    <table
+                        {...getTableProps()}
+                        role="table"
+                        tabIndex={0}
+                        className="table-fixed border-solid border-2 border-green-800 border-spacing-0 shadow-2xl box-border ">
+                        <thead>
+                            {headerGroups.map(
+                                (headerGroup, headerGroupIndex) => (
+                                    <tr
+                                        key={headerGroupIndex}
+                                        {...headerGroup.getHeaderGroupProps()}>
+                                        {headerGroup.headers.map(
+                                            (column, columnIndex) => (
+                                                <th
+                                                    tabIndex={0}
+                                                    className="border-solid border-2 border-green-800 bg-custom-500 text-white py-1 pl-2 w-[100px] lg:w-[130px] 2xl:w-[300px] text-xs lg:text-base whitespace-nowrap"
+                                                    key={columnIndex}
+                                                    {...column.getHeaderProps(
+                                                        column.getSortByToggleProps()
+                                                    )}
+                                                    aria-label={`Sort by ${column.render(
+                                                        "Header"
+                                                    )}`}
+                                                    aria-sort={
+                                                        column.isSorted
+                                                            ? column.isSortedDesc
+                                                                ? "descending"
+                                                                : "ascending"
+                                                            : "none"
+                                                    }>
+                                                    {column.render("Header")}{" "}
+                                                    <button
+                                                        className="text-sm text-custom-502 pl-1"
+                                                        aria-label={`Sort by ${
+                                                            column.isSorted
+                                                                ? column.isSortedDesc
+                                                                    ? "descending"
+                                                                    : "ascending"
+                                                                : "ascending"
+                                                        }`}>
+                                                        {!column.isSorted && (
+                                                            <i className="fa fa-sort"></i>
+                                                        )}
+                                                        {column.isSorted &&
+                                                            !column.isSortedDesc &&
+                                                            "🔼"}
+                                                        {column.isSorted &&
+                                                            column.isSortedDesc &&
+                                                            "🔽"}
+                                                    </button>
+                                                </th>
+                                            )
+                                        )}
+                                    </tr>
+                                )
+                            )}
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {page.map((row, rowIndex) => {
+                                prepareRow(row);
+                                return (
+                                    <tr
+                                        data-testid="tr-employee"
+                                        tabIndex={0}
+                                        className="even:bg-custom-501 hover:bg-custom-503 hover:text-white "
+                                        key={rowIndex}
+                                        {...row.getRowProps()}>
+                                        {row.cells.map((cell, cellIndex) => {
+                                            return (
+                                                <td
+                                                    className="text-center border-solid border-2 border-green-800 p-1 text-xs lg:text-base truncate ..."
+                                                    key={cellIndex}
+                                                    {...cell.getCellProps()}>
+                                                    {cell.render("Cell")}
+                                                </td>
+                                            );
+                                        })}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <Pagination
+                    pageSize={pageSize}
+                    employeeProfile={employeeProfile}
+                    combinedData={combinedData}
+                    pageIndex={pageIndex}
+                    pageCount={pageCount}
+                    nextPage={nextPage}
+                    previousPage={previousPage}
+                    gotoPage={gotoPage}
+                />
             </div>
-            <Pagination
-                pageSize={pageSize}
-                employeeProfile={employeeProfile}
-                combinedData={combinedData}
-                pageIndex={pageIndex}
-                pageCount={pageCount}
-                nextPage={nextPage}
-                previousPage={previousPage}
-                gotoPage={gotoPage}
-            />
         </div>
     );
 }
